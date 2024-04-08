b5eb476decfdd26e8538e009dfa3e339d6295967
NomikLee <nomiklee@icloud.com>
2024-04-08 13:07:12 +0800

    Initial Commit

---
+++ ElectronicBooksNomik/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ElectronicBooksNomik
+//
+//  Created by Pinocchio on 2024/4/8.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
