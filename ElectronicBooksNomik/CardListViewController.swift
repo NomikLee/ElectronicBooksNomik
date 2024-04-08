8075f35a9ab0fc253fd1cc639ecde261a56e0309
NomikLee <nomiklee@icloud.com>
2024-04-08 14:07:14 +0800

    eBook add tab bar

---
+++ ElectronicBooksNomik/CardListViewController.swift
@@ -0,0 +1,29 @@
+//
+//  CardListViewController.swift
+//  ElectronicBooksNomik
+//
+//  Created by Pinocchio on 2024/4/8.
+//
+
+import UIKit
+
+class CardListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ ElectronicBooksNomik/MainViewController.swift
@@ -0,0 +1,22 @@
+//
+//  MainViewController.swift
+//  ElectronicBooksNomik
+//
+//  Created by Pinocchio on 2024/4/8.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemFill
+    }
+
+}
+
+#Preview("mainVC", body: {
+    MainViewController()
+})
+
